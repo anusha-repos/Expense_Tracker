7b0cb1da22c4aa07490a49b7ce211705ddacb112
Anusha <anusha@example.com>
2026-07-29 08:17:37 +0530

    Fix axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 export default API;
